5d3399295a4a323eed8a095c05a119c746ff72af
chima <chimaataman@gmail.com>
2020-06-20 05:31:57 +0100

    bind payments.db to sqlalchemy

--- bin/setup.py
+++ bin/setup.py
@@ -7,11 +7,15 @@ from flask_bcrypt import Bcrypt
 
 base_dir = os.path.dirname(__file__)
 notes_db_path = os.path.join(base_dir, 'notes', 'database')
+payments_db_path = os.path.join(base_dir, 'payments', 'database')
 
 # todo: call db creation scripts from here (migration?)
 #       run setup {files?} of each individual app from here
 
-sqlalchemy_binds = {'notes': 'sqlite:///' + os.path.join(notes_db_path, 'notes.db')}  # add other db to this dictionary
+sqlalchemy_binds = {
+                        'notes': 'sqlite:///' + os.path.join(notes_db_path, 'notes.db'),
+                        'payments': 'sqlite:///' + os.path.join(payments_db_path, 'payments.db'),
+                    }
 
 connex_app = connexion.App(__name__, specification_dir=base_dir)
 app = connex_app.app
@@ -19,6 +23,7 @@ app = connex_app.app
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_BINDS'] = sqlalchemy_binds
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
