025638e1725745cdfe9004084ca423cb4475466a
chima <chimaataman@gmail.com>
2020-07-05 20:41:20 +0100

    minor refactoring

--- notes/operations.py
+++ notes/operations.py
@@ -175,15 +175,6 @@ def get_headers():
 
 @app.route('/update_server', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('/home/AtamanBC/Convergence/')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
-
-def webhook():
     if request.method != 'POST':
         return 'OK'
     else:
