6798397326f22a06de97b5195c439e76b11760c2
chima <chimaataman@gmail.com>
2020-07-10 05:30:56 +0100

    refactoring for deployment on elastic beanstalk

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from setup import base_dir
 
 connex_app.add_api(os.path.join(base_dir, 'payments', 'payments_spec.yaml'))
 connex_app.add_api(os.path.join(base_dir, 'notes', 'notes_spec.yaml'))
+application = connex_app.app
 
 if __name__ == '__main__':
-    connex_app.run(debug=True, port=3700)
+    application.run(debug=True, port=3700)
 
