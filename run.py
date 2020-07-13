baac7b4f0703ccd11a305a9be4508e4cabb7ae03
cAtaman <chimaataman@gmail.com>
2020-07-13 21:45:53 +0000

    fix merge error

--- run.py
+++ run.py
@@ -7,9 +7,4 @@ connex_app.add_api(os.path.join(base_dir, 'notes', 'notes_spec.yaml'))
 application = connex_app.app
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    connex_app.run(debug=True, port=3700)
-=======
     application.run(debug=True, port=3700)
-
->>>>>>> 40d2841371adf9a5f9c6ad81d1b42de48f80b039
