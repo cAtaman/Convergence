fa155763ec45b4938b9f54785b8f4318c670e9bd
chima <chimaataman@gmail.com>
2020-08-03 17:18:39 +0100

    merge branch 'master' into aws-eb-deploy

--- run.py
+++ run.py
@@ -4,12 +4,6 @@ from setup import base_dir
 
 connex_app.add_api(os.path.join(base_dir, 'payments', 'payments_spec.yaml'))
 connex_app.add_api(os.path.join(base_dir, 'notes', 'notes_spec.yaml'))
-
-if not os.path.exists(os.path.join(os.path.expanduser('~'), 's3backups')):
-    os.makedirs(os.path.join(os.path.expanduser('~'), 's3backups'))
-with open(os.path.join(os.path.expanduser('~'), 's3backups', 'working_dir'), 'w') as ff:
-    ff.write(os.getcwd())
-
 application = connex_app.app
 
 
