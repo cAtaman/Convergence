83ce7d8a9a68db47d7b4188a1c01245300f0507c
chima <chimaataman@gmail.com>
2020-08-03 16:49:48 +0100

    remove __bind_key__ declaration in notes model

--- notes/models.py
+++ notes/models.py
@@ -1,30 +1,14 @@
 from setup import db, ma
 
 
-# Asset class
-class Product(db.Model):
-    __bind_key__ = 'notes'
-    __tablename__ = 'product'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True)
-
-
-# Notes -- new functionality
+# Notes
 class Note(db.Model):
-    __bind_key__ = 'notes'
     __tablename__ = 'note'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     datetime = db.Column(db.String)
     user = db.Column(db.String(100))
     content = db.Column(db.String)
-
-
-# Product Schema
-class ProductSchema(ma.ModelSchema):
-    class Meta:
-        model = Product
-        sqla_session = db.session
 
 
 # Notes Schema
